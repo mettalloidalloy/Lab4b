eee2aeddc4bb8c369e073bdd5b06d11db6a0a5a4
Alexander Krock <mettalloid@gmail.com>
2020-07-07 15:18:56 +0000

    Reworked entire code

--- main.cs
+++ main.cs
@@ -1,42 +1,40 @@
 using System;
-					
-class MainClass
+using System.Linq;
+
+class MainClass 
 {
-	public static void Main()
-	{
-		int height;
-		int weight;
-		int bmi;
-		
-		Console.WriteLine("Enter your height in inches: ");
-		height = Convert.ToInt32(Console.ReadLine());
-		
-		Console.WriteLine("Enter your weight in pounds: ");
-		weight = Convert.ToInt32(Console.ReadLine());
-
-		
-		bmi = (weight * 703)/(height * height);
-
-    if (bmi < 18.5)
+    public static void Main (string[] args) 
     {
-		Console.WriteLine("Your BMI is {0}. You are underweight.",bmi);
-		}
-
-		if (bmi >= 18.5 && bmi <= 24.9 )
-		{
-		Console.WriteLine("Your BMI is {0}. You are normal weight.",bmi);	
-		}	
-		
-		if (bmi >= 25 && bmi <= 29.9 )
-    {
-		Console.WriteLine("Your BMI is {0}. You are overweight.",bmi);
+       int i = 0;
+       while (i < 10) 
+       {
+           Console.WriteLine("Enter your height:");
+           int height = Convert.ToInt32(Console.ReadLine());
+           Console.WriteLine("Enter your weight:");
+           int weight = Convert.ToInt32(Console.ReadLine());
+           double bmi = calculateBMI(height, weight);
+           BMIResults(bmi);
+           i++;
+       }
     }
 
-    if (bmi > 30)
+    public static double calculateBMI (int height, int weight) 
     {
-		Console.WriteLine("Your BMI is {0}. You are Obese.",bmi);
-		}
-				
-			
-	}
+        double bmi = (weight * 703) / (height * height);
+        return bmi;
+    }
+
+    public static void BMIResults(double bmi) 
+    {
+        if (bmi >= 30) 
+        {
+            Console.WriteLine("Obese");
+        } else if (bmi >= 25) {
+            Console.WriteLine("Overweight");
+        } else if (bmi >= 18.5) {
+            Console.WriteLine("Normal");
+        } else {
+            Console.WriteLine("Underweight");
+        }
+    }
 }
